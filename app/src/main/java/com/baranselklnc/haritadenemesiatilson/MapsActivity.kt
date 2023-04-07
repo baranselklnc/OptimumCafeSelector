b9d9608838d78ffca2e073b5451df2a806b62bc0
Baransel <49001016+baranselklnc@users.noreply.github.com>
2023-04-07 21:47:56 +0300

    getFromLocation is deprecated

--- app/src/main/java/com/baranselklnc/haritadenemesiatilson/MapsActivity.kt
+++ app/src/main/java/com/baranselklnc/haritadenemesiatilson/MapsActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.*
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
